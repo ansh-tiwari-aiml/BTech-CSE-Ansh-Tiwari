c56711b96827bb958ad584194876eff68db06d63
ansh-tiwari-aiml <hypebhai7@gmail.com>
2026-08-29 12:08:51 +0530

    Create 03_basic_datatypes.cpp

---
+++ 02-hackerrank-cpp/03_basic_datatypes.cpp
@@ -0,0 +1,35 @@
+//Topic : Introduction
+//Question : Basic Data Types
+//Link : https://hr.gs/4ljk3
+//Date : 29-june-2026
+//Author : Ansh Tiwari
+
+#include<iostream>
+using namespace std;
+int main() {
+ int a;
+ long b;
+ char c;
+ float d;
+ double e;
+ scanf ( "%d %ld %c %f %lf" ,&a ,&b ,&c ,&d ,&e);
+ printf ("%d\n" , a);
+ 
+         printf("%ld\n" , b);
+         
+              printf("%c\n" , c);
+              
+                     printf("%f\n" , d);
+                     
+                         printf("%lf" , e);
+                         
+
+/*cin>>a>>b>>c>>d>>e;
+cout<<a<<"\n";
+cout<<b<<"\n";
+cout<<c<<"\n";
+cout<<d<<"\n";
+cout<<e<<"\n";
+*/
+   return 0;
+}
