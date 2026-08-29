2ff99c1c3812414235006a6bdd00114319eb1b38
ansh-tiwari-aiml <hypebhai7@gmail.com>
2026-08-29 12:07:43 +0530

    Create 02_input_and_output.cpp

---
+++ 02-hackerrank-cpp/02_input_and_output.cpp
@@ -0,0 +1,14 @@
+//Topic : Introduction
+//Question : Input and Output
+//Link : https://hr.gs/4kks2
+//Date : 28-june-2026
+//Author : Ansh Tiwari
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+ cin>> a>>b>>c;
+    cout<< a+b+c;
+    return 0;
+}
