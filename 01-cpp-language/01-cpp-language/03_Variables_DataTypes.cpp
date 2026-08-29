48509a690cb24b08b3b865034800d1dcf389821d
ansh-tiwari-aiml <hypebhai7@gmail.com>
2026-08-29 11:27:56 +0530

    Create 03_Variables_DataTypes.cpp

---
+++ 01-cpp-language/01-cpp-language/03_Variables_DataTypes.cpp
@@ -0,0 +1,16 @@
+//Topic : Variables and Data Types
+//Name : 03_Variables_DataTypes.cpp
+//Author : Ansh Tiwari
+//Date : 28-june-2026
+//Course : Btech Cse
+
+#include<iostream>
+using namespace std;
+int main(){
+    //int a = 14;
+    //int b = 15;
+    int a = 14; int b = 15;
+    float pi = 3.14;
+    char c ='d';
+    cout<<"This is variables and Data Types.In here the value of a is "<<a<<"\n The value of b is "<<b;cout<<"in The value of pi is "<<pi; cout<<"in the value of c is "<<c;
+    return 0; }
