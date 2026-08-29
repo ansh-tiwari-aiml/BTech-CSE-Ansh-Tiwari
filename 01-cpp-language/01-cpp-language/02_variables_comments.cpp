ae1e08d636cea7a4bf89011fbf566f26fbbd0b57
ansh-tiwari-aiml <hypebhai7@gmail.com>
2026-08-29 11:27:00 +0530

    Create 02_variables_comments.cpp

---
+++ 01-cpp-language/01-cpp-language/02_variables_comments.cpp
@@ -0,0 +1,21 @@
+// Topic : Variables and Comments
+// Name : 02_variables_comments.cpp
+// Author : Ansh Tiwari 
+// Date : 27-June-2026
+// Course : BTech CSE
+
+
+#include<iostream>
+using namespace std;
+//This program was created by Ansh Tiwari 
+/*this 
+is
+a
+multi
+line
+comment */
+int main( ){
+    int sum = 6; 
+    cout<< "hello world"<< sum;
+    return 0; 
+}
