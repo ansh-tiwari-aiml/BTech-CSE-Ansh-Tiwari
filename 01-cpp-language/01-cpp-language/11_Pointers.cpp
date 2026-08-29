a94de76769d7c79d8b76db2fee1146b6d89fc91c
ansh-tiwari-aiml <hypebhai7@gmail.com>
2026-08-29 11:40:28 +0530

    Create 11_Pointers.cpp

---
+++ 01-cpp-language/01-cpp-language/11_Pointers.cpp
@@ -0,0 +1,25 @@
+//Topic : Pointers
+//Name : 11_Pointers.cpp
+//Author : Ansh Tiwari
+//Date : 11-July-2026
+//Course : Btech Cse 
+#include<iostream>
+using namespace std;
+int main(){
+    //What is a pointer ? ---> Data type which holds the address of other data types
+    int a = 3 ;
+    int* b;
+    b = &a;
+    //&--> (Address of) operator
+    cout<<"The address of a is "<<&a<<endl;
+    cout<<"The address of a is"<<b<<endl;
+    //*---> (Value at) Dereference operator
+    cout<<"The value at address b is"<<*b<<endl;
+    //pointer to pointer 
+    int**c=&b;
+    cout<<"The address of b is"<<&b<<endl;
+    cout<<"The address of b is"<<c<<endl;
+    cout<<"The value at address c is"<<*c<<endl;
+    cout<<"The value at address Value_at(Value_at(c)) is "<<**c<<endl;
+    return 0 ;
+}
