8eca5d727d3ffa58d369492b1639f7eadb536d10
ansh-tiwari-aiml <hypebhai7@gmail.com>
2026-08-29 11:28:55 +0530

    Create 04_input_and_output.cpp

---
+++ 01-cpp-language/01-cpp-language/04_input_and_output.cpp
@@ -0,0 +1,17 @@
+//Topic : Input and Output
+//Name : 04_input_and_output.cpp
+//Author : Ansh Tiwari
+//Date : 28-june-2026
+//Course : BTech Cse
+
+#include<iostream>
+using namespace std;
+int main() {
+    int num1, num2;
+    cout<<"Enter the value of num1: \n";
+    cin>>num1;
+    cout<<"Enter the value of num2: \n";
+    cin>>num2;
+    cout<<"The sum is "<< num1+num2;
+    return 0; 
+}
