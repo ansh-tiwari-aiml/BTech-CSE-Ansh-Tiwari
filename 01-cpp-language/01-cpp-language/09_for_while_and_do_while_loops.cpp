03bd742f64163edf17e6d043acd9946996685f60
ansh-tiwari-aiml <hypebhai7@gmail.com>
2026-08-29 11:36:30 +0530

    Create 09_for_while_and_do_while_loops.cpp

---
+++ 01-cpp-language/01-cpp-language/09_for_while_and_do_while_loops.cpp
@@ -0,0 +1,68 @@
+//Topic:For,while and do-while loops
+//Name:09_for_while_and_do_while_loops.cpp
+//Author:Ansh Tiwari
+//Date:9-july-2026
+//Course:Btech Cse
+
+#include<iostream>
+using namespace std;
+int main() {
+    /* Loop in c++
+    There are two types of loop in c++:
+    1. for loop
+    2. while loop
+    3. for while loop */
+    
+    /* for loop in c++ */
+    // int i = 1;
+    //cout<<i;
+    //i++;
+    //syntax for for loop 
+    //for (initialization; condition ; updation)
+    //{
+        //loop body (c++code);
+        //}
+        //for (int i = 1; i<=40; i++)
+        //{
+            // /*code*/
+            //cout<<i<<endl;
+            //}
+       //Example of infinite for loop 
+       //for (int i = 1; 34<=40; i++)
+       //{
+           // /*code */
+           //cout<<i<<endl;
+           //}
+           /*while loop in c++ */
+           //syntax ;
+           //while (condition)
+           //{
+               //c++ statements;
+               //}
+         //printing 1 to 40 using while loop 
+         //int i = 1;
+         //while (i<=40){
+             //cout<<i<<endl;
+             //i++;
+             //}
+             //example of inifinte while loop 
+             //int i=1;
+             //while (true)
+             //{
+                 //cout<<i<<endl;
+                 //i++;
+                 //}
+                 /* do while loop in c++ */
+                 //syntax ;
+                 //do
+                 //{
+                     //c++ statements ;
+                     //} while (condition);
+                     //printing 1 to 40 using while loop 
+                     //int i = 1;
+                     //do {
+                       //cout<<i<<endl;
+                       //i++;
+                       //} while(false);
+                       return 0;
+}
