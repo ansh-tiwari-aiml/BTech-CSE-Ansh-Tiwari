beba7ce46ea49bd152a3109293f16c40929b3745
ansh-tiwari-aiml <hypebhai7@gmail.com>
2026-08-29 11:25:42 +0530

    Create 01_hello_ansh_tiwari_world.cpp

---
+++ 01-cpp-language/01-cpp-language/01_hello_ansh_tiwari_world.cpp
@@ -0,0 +1,11 @@
+// Topic : Hello Ansh Tiwari World
+// Name : 01_hello_ansh_tiwari_world.cpp
+// Author : Ansh Tiwari 
+// Date : 27-June-2026
+// Course : BTech CSE
+
+#include<iostream>
+using namespace std;
+int main( ) {
+cout<<"Hello Ansh tiwari world";
+return 0; }
