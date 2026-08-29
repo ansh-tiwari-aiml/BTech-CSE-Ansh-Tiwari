3654afb245f50c57eee618cc4d520b27ec415f7e
ansh-tiwari-aiml <hypebhai7@gmail.com>
2026-08-29 12:06:37 +0530

    Create 01_say_hello_world.cpp

---
+++ 02-hackerrank-cpp/01_say_hello_world.cpp
@@ -0,0 +1,11 @@
+//Topic : Introduction
+//Question : Say "Hello, World!"
+//Link : https://hr.gs/4jadb
+//Date : 28-June-2026
+//Author : Ansh Tiwari
+
+#include<iostream>
+using namespace std;
+int main ( ) {
+cout<<"Hello, World!";
+return 0; }
